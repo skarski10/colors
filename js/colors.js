fdb612a2fe34a4e2c1a9d61b3123074276881e27
Keely Glenn and Kory Skarbek <keely@>
2017-01-10 10:00:14 -0800

    Create background color page with changing background

---
+++ js/colors.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
